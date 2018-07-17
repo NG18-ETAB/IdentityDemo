e1d121937c8963299170261701de54eca05046b6
Adrian Lozano <adrian@xenotype.com>
2018-07-17 16:47:21 +0200

    Demo different Authorize Role combinations

--- IdentityDemo/Controllers/HomeController.cs
+++ IdentityDemo/Controllers/HomeController.cs
@@ -6,25 +6,57 @@ using System.Web.Mvc;
 
 namespace IdentityDemo.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
+        [AllowAnonymous]
         public ActionResult Index()
         {
+            ViewBag.Title = "Index";
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Default()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            ViewBag.Title = "Default";
+            return View("Index");
         }
 
-        public ActionResult Contact()
+        [Authorize(Roles = "Admin")]
+        public ActionResult Admin()
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            ViewBag.Title = "Admin";
+            return View("Index");
         }
+
+        [Authorize(Roles = "Editor")]
+        public ActionResult Editor()
+        {
+            ViewBag.Title = "Editor";
+            return View("Index");
+        }
+        
+        [Authorize(Roles = "Editor, Admin")]
+        public ActionResult EditorOrAdmin()
+        {
+            ViewBag.Title = "Editor or Admin";
+            return View("Index");
+        }
+
+        [Authorize(Roles = "Editor")]
+        [Authorize(Roles = "Admin")]
+        public ActionResult EditorAndAdmin()
+        {
+            ViewBag.Title = "Editor and Admin";
+            return View("Index");
+        }
+
+        [Authorize(Users = "user@lexicon.se")]
+        public ActionResult UserIdentity()
+        {
+            ViewBag.Title = "Editor and Admin";
+            return View("Index");
+        }
+
     }
 }
